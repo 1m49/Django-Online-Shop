d972bcf1fba960100857d100702e5dc239020c53
imanakbari <89096381+1m49@users.noreply.github.com>
2023-09-09 22:46:29 +0330

    show comments in product detail from DB

--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import ListView , DetailView
+from django.views.generic import ListView, DetailView
 from .models import Product
+
 
 # Product List
 class ProductListView(ListView):
@@ -8,12 +9,9 @@ class ProductListView(ListView):
     template_name = 'products/product_list.html'
     context_object_name = 'products'
 
+
 # Product Detail
 class ProductDetailView(DetailView):
     model = Product
     template_name = 'products/product_detail.html'
     context_object_name = 'product'
-
-
-
-
