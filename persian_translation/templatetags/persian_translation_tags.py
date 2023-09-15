4c4b9788ae9f554fe0f0bf0ed717af468aade265
imanakbari <89096381+1m49@users.noreply.github.com>
2023-09-15 22:05:40 +0330

    Separate Persian numbers with comma

--- persian_translation/templatetags/persian_translation_tags.py
+++ persian_translation/templatetags/persian_translation_tags.py
@@ -1,4 +1,5 @@
 from django import template
+from persian_tools import separator
 
 register = template.Library()
 
@@ -8,3 +9,8 @@ def translate_number(value):
     value = str(value)
     english_to_persian = value.maketrans('1234567890', '۱۲۳۴۵۶۷۸۹۰')
     return value.translate(english_to_persian)
+
+
+@register.filter
+def number_separator(value):
+    return separator.add(value)
