9bac5dd3715cd019cb91fdbc2860a142c3ae45f4
imanakbari <89096381+1m49@users.noreply.github.com>
2023-09-16 18:53:54 +0330

    register Order & OrderItem in admin.py

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Order, OrderItem
 
-# Register your models here.
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    fields = ('user', 'first_name', 'last_name', 'phone_number', 'datetime_created', 'is_paid')
+
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    fields = ('order', 'product', 'quantity', 'price')
