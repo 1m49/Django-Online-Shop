ca84d23368ea40200bfd9330eac56789575d6ee4
imanakbari <89096381+1m49@users.noreply.github.com>
2023-09-16 18:48:31 +0330

    create model order

--- orders/models.py
+++ orders/models.py
@@ -1,4 +1,24 @@
 from django.db import models
+from django.conf import settings
+from products.models import Product
+
+
+class Order(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    is_paid = models.BooleanField()
+
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=11)
+    address = models.CharField(max_length=700)
+
+    order_notes = models.CharField(max_length=700, blank=True)
+
+    datetime_created = models.DateTimeField(auto_now_add=True)
+    datetime_modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'Order {self.id}'
 
 
 
