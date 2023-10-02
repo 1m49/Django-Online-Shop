0ac49661906b39e3951131b49930953b297ef12e
imanakbari <89096381+1m49@users.noreply.github.com>
2023-10-02 14:15:40 +0330

    add messages for order create

--- orders/views.py
+++ orders/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 from .forms import OrderForm
 
@@ -14,5 +15,6 @@ def order_create_view(request):
             order_obj = order_form.save(commit=False)
             order_obj.user = request.user
             order_obj.save()
+        messages.success(request, 'سفارش شما با موفقیت ثبت شد ✅', 'success')
 
     return render(request, 'orders/order_create.html', context={'form': order_form})
